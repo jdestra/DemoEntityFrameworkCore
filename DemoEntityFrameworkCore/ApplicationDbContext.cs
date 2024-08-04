a4ddbb2e70acbc68740b120b5472a002655e8385
Jorge Diestra <diestradiestrajorge@gmail.com>
2024-08-04 14:11:50 +0200

    Add project files.

---
+++ DemoEntityFrameworkCore/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoEntityFrameworkCore
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer(
+                "Server=LAPTOP-UL8JHP4F\\SQLDEV;Initial Catalog=DemoEFCoreUdemy;Integrated Security=True; TrustServerCertificate=True");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<Persona> Personas { get; set; }
+    }
+}
